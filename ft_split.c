cceff8973be4d6574cb5d71e73d38d5a5eaddc45
tayqiter00 <tayqiter00@gmail.com>
2024-07-05 01:56:33 +0800

    added ft_split

---
+++ ft_split.c
@@ -0,0 +1,82 @@
+#include "libft.h"
+// #include <stdio.h>
+
+static size_t  word_count(char const *s, char c)
+{
+    int     i;
+    size_t  count;
+
+    i = -1;
+    count = 0;
+    while (s[++i])
+    {
+        if (s[i] != c && (s[i + 1] == c || s[i + 1] == '\0'))
+            count++;
+    }
+    return (count);
+}
+
+static char **fill_str(char const *s, char c, size_t word_count, char **buffer)
+{
+    int     i;
+    int     start;
+    int     end;
+
+    i = -1;
+    start = 0;
+    while (++i < word_count)
+    {
+        while (s[start] && s[start] == c)
+            start++;
+        end = start;
+        while (s[end] != c)
+            end++;
+        buffer[i] = ft_substr(s, start, end - start);
+        if (!buffer[i])
+        {
+            while (i > 0)
+                free(buffer[--i]);
+            free(buffer);
+            return (NULL);
+        }
+        start = end;
+    }
+    buffer[word_count] = NULL;
+    return (buffer);
+}
+
+char    **ft_split(char const *s, char c)
+{
+    char    **buffer;
+    size_t num_of_words;
+
+    if (!s)
+        return (NULL);
+    num_of_words = word_count(s, c);
+    buffer = malloc((num_of_words + 1) * sizeof(char *)); 
+    if (!buffer)
+        return (NULL);
+    return (fill_str(s, c, num_of_words, buffer));
+}
+
+// int main()
+// {
+//     int i = 0;
+//     char    *str = "This is   an example ";
+//     char    **split;
+
+//     split = ft_split(str, ' ');
+//     while (split[i] != NULL)
+//     {
+//         printf("%s\n", split[i]);
+//         i++;
+//     }
+//     i = 0;
+//     while (split[i] != NULL)
+//     {
+//         free(split[i]);
+//         i++;
+//     }
+//     free(split);
+//     return (0);
+// }
--- libft.h
+++ libft.h
@@ -29,4 +29,11 @@ char    *ft_strnstr(const char *big, const char *little, size_t len);
 int     ft_atoi(const char *nptr);
 void    *ft_calloc(size_t nmemb, size_t size);
 char    *ft_strdup(const char *s);
+
+/* PART 2 - ADDITIONAL FUNCTIONS */
+char    *ft_substr(char const *s, unsigned int start, size_t len);
+char    *ft_strjoin(char const *s1, char const *s2);
+char    *ft_strtrim(char const *s1, char const *set);
+char    **ft_split(char const *s, char c);
+
 #endif
